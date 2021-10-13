ce83e839fdd03e7ec506a12e451091f4b706bcb8
Willian <guns945@gmail.com>
2021-10-13 14:21:54 -0300

    Refactoring of prep1 for paralelization.

--- prep1.py
+++ prep1.py
@@ -1,8 +1,8 @@
 import time
-import numpy as np
 from functools import reduce
 import sys
 import random
+from io import StringIO
 
 # max_features = 1372 # for w1
 # max_features = 500 # for w2
@@ -31,11 +31,11 @@ output_file = sys.argv[3]
 
 # Generate random features' ids
 random.seed(RANDOM_STATE)
-ids = random.sample(range(1,MAX_ID+1), N_FEATURES)
+ids = random.sample(range(MAX_ID), N_FEATURES)
 
 # Allocate npy data
-num_lines = sum(1 for line in open(wells_file, 'r'))-1  # last line is empty
-output = np.empty((num_lines, N_FEATURES + N_INFO))
+num_lines = sum(1 for line in open(wells_file, 'r')) - 1  # last line is empty
+output = []
 
 t2 = time.time()
 
@@ -43,34 +43,39 @@ t2 = time.time()
 with open(wells_file, 'r') as f:
     # First line is labels only
     all_labels = f.readline()
-    
-    for i in range(num_lines):
+
+    for line in f.readlines():
+        s = StringIO()
+
         # Move selected features features
-        fields = f.readline().split(',')
-        for j in range(N_FEATURES):
-            output[i][j] = fields[ids[j] - 1]
+        fields = line.split(',')
+        for i in range(N_FEATURES):
+            s.write(str(fields[ids[i]]) + ",")
 
         # Move info data fields
-        output[i][N_FEATURES:] = fields[max_features:max_features+N_INFO]
+        for f in fields[max_features:max_features + N_INFO]:
+            s.write(str(f) + ",")
+
+        # Add to output without trailing comma
+        output.append(s.getvalue()[:-1])
 
 t3 = time.time()
 
 # Write results
 with open(output_file, 'w') as f:
     all_labels = all_labels.replace('\n', '').split(',')
-    labels = reduce(lambda rem, id: rem + ',' + all_labels[id - 1], ids,
-                    "")[1:]
-    labels += reduce(lambda rem, id: rem + ',' + all_labels[id - 1],
-                     range(max_features+1, max_features + N_INFO + 1), "")
+    labels = reduce(lambda rem, id: rem + ',' + all_labels[id], ids, "")[1:]
+    labels += reduce(lambda rem, id: rem + ',' + all_labels[id],
+                     range(max_features, max_features + N_INFO), "")
     print(labels, file=f)
     for i in range(num_lines):
-        print(reduce(lambda x, y: str(x) + ',' + str(y), output[i]), file=f)
+        print(output[i], file=f)
 
 t4 = time.time()
 
 # Write profiling times
-with open('prep1-times.log', mode='a') as f:
-        print("prep: " + str(t2 - t1), file=f)
-        print("process: " + str(t3 - t2), file=f)
-        print("write: " + str(t4 - t3), file=f)
-        print("", file=f)
+# with open('prep1-times.log', mode='a') as f:
+print("prep: " + str(t2 - t1))
+print("process: " + str(t3 - t2))
+print("write: " + str(t4 - t3))
+# print("", file=f)
