26674de87cab99418a3110ce0275e6f60ac0c38c
Willian <guns945@gmail.com>
2022-01-23 11:14:49 -0300

    Added petro feature selection to main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import multiprocessing as mp
+import pandas as pd
 import time
 
 import header
 import expand
-# import petro
+import petro
 # import apply3
 
 # Constants
@@ -12,17 +13,24 @@ INIT_IT = 2
 def main():
     iterations = 2
     window = 3
-
+    
     for i in range(iterations):
         print("Preparing header")
-        nwells = header.prepare_header(window)
+        str_nwells = header.prepare_header(window)
+        
         print("Expanding points")
-        nwells = nwells + expand.data_aug(i + 1, window)
-        print(len(nwells))
-        # features_sets = petro(nwells)
+        str_nwells += "\n" + expand.data_aug(i + 1, window) # param sA passed by global variable
+        print(len(str_nwells))
+        with open('tmp_data/nwells.csv', mode='w') as f:
+            f.write(str_nwells)
+
+        print("Performing feature selection")
+        features_sets = petro.get_features_sets(str_nwells)
+        print(features_sets)
         # f = prep(features_sets)...
         # v = apply3(nwells, f)
         # predition_values = prep2(v)
+        # sA =+ values?
 
         # write(nwells)
         # write(predition_values)
--- petro.py
+++ petro.py
@@ -3,15 +3,15 @@ import numpy as np
 import sys
 import os
 import random
+from io import StringIO
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import LeaveOneGroupOut
 import lightgbm as lgb
 
-
 # Parameters
 LABEL_COLUMN_NAME = 'phi'
-UNWANTED_COLUMNS = ['real','well','rho','vs','vp']
+UNWANTED_COLUMNS = ['real', 'well', 'rho', 'vs', 'vp']
 
 RANDOM_STATE = 1
 
@@ -45,50 +45,65 @@ def eval_bootstrap(df, features):
         v = []
         n = 0
         for i in np.array(df['real'][val]):
-            if i == 1: # if REAL
+            if i == 1:  # if REAL
                 v.append(n)
             n = n + 1
         v = np.array(v)
         lgb_train = lgb.Dataset(X[train], y[train])
         #lgb_eval = lgb.Dataset(X[val], y[val], reference=lgb_train)
         lgb_eval = lgb.Dataset(X[v], y[v], reference=lgb_train)
-        regressor = lgb.train(params, lgb_train, verbose_eval=False, num_boost_round=100, valid_sets=lgb_eval, early_stopping_rounds=30)
+        regressor = lgb.train(params,
+                              lgb_train,
+                              verbose_eval=False,
+                              num_boost_round=100,
+                              valid_sets=lgb_eval,
+                              early_stopping_rounds=30)
         pred = regressor.predict(X[v])
         rmse = np.sqrt(np.mean((pred - y[v])**2))
         mae = mean_absolute_error(pred, y[v])
-        a.insert(len(a), rmse)
-        b.insert(len(b), mae)
-    return np.mean(a),np.mean(b)
+        a.append(rmse)
+        b.append(mae)
+    return np.mean(a), np.mean(b)
 
 
-# Reads dataset
-df = pd.read_csv(sys.argv[1])
-df.dropna(axis=0, subset=[LABEL_COLUMN_NAME], inplace=True)
+def get_features_sets(str_nwells):
+    # Reads dataset
+    str_nwells = StringIO(str_nwells)
+    str_nwells.seek(0)
+    df = pd.read_csv(str_nwells)
+    df.dropna(axis=0, subset=[LABEL_COLUMN_NAME], inplace=True)
 
-RANDOM_STATE = 1
-all_features = list(df.columns)
-for x in UNWANTED_COLUMNS + [LABEL_COLUMN_NAME]:
-    all_features.remove(x)
+    RANDOM_STATE = 1
+    all_features = list(df.columns)
+    for x in UNWANTED_COLUMNS + [LABEL_COLUMN_NAME]:
+        all_features.remove(x)
 
-f = ['X', 'Y', 'depth']
-i = 0
-for f1 in all_features:
-    if i == 10: break
-    if f1 in f: continue
-    k = 1000
-    x = f1
-    i = i + 1
-    j = 0
-    for f2 in all_features:
-         if f2 in f: continue
-         j = j + 1
-         f.insert(len(f), f2)
-         A,B = eval_bootstrap(df, f)
-         print("%s,%f,%f" % (f,A,B))
-         z = A
-         f.remove(f2)
-         sys.stdout.flush()
-         if z < k:
-             x = f2
-             k = z
-    f.insert(len(f), x)
+    print("[petro] Starting features set search")
+    f = ['X', 'Y', 'depth']
+    i = 0
+    results = []
+    for f1 in all_features:
+        if i == 10: break
+        if f1 in f: continue
+        k = 1000
+        x = f1
+        i = i + 1
+        j = 0
+        for f2 in all_features:
+            if f2 in f: continue
+            j = j + 1
+            if j == 5: break # FOR DEBUGING => less iterations
+            f.append(f2)
+            print(f)
+            A, B = eval_bootstrap(df, f)
+            # s.write(f"{f},{A},{B}")
+            results.append([f, A, B])
+            z = A
+            f.remove(f2)
+            sys.stdout.flush()
+            if z < k:
+                x = f2
+                k = z
+        f.append(x)
+
+    return results
