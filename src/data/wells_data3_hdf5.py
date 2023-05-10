407de2aa08a74e496d0468e4e7c9d26d2a6261c3
Daniel Campos <danielcampos14@hotmail.com.br>
2023-05-10 09:11:49 -0300

    changing import again

--- src/data/wells_data3_hdf5.py
+++ src/data/wells_data3_hdf5.py
@@ -9,7 +9,7 @@ import numpy as np
 import pathlib
 from tqdm import tqdm
 
-from alg import common
+import alg.common
 
 def porosity_points_py2hdf5(porosity_file_path, hdf5_file_path, hypercube_shape, chunk_shape,
                             real_points):
