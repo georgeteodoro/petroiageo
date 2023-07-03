a540d8b92f9f7a2cc389089a606b27524eadfbf5
Daniel Campos <danielcampos14@hotmail.com.br>
2023-07-03 10:41:27 -0300

    updating parameters of script

--- src/data/wells_data_sd.sh
+++ src/data/wells_data_sd.sh
@@ -6,11 +6,11 @@
 #SBATCH --account=petrobrasiageo
 #SBATCH --exclusive         #Utilização exclusiva dos nós
 
-#module load python/3.9.6
+module load python/3.9.6
 
 source ../../../meu_python/bin/activate
-FEAT_FILE='../../data/POV/features/NEAR.npy'
-POR_FILE='../../data/POV/raw/porosity-canal.txt'
-TARGET_HDF5_FILE='../../data/POV/processed/porosity_data.h5'
+FEAT_FILE='./../data/ANP/features/area1/Franco_florin_buzios_28_09-2_azimuth_area1_.npy'
+POR_FILE='../../data/ANP/processed/area_1_porsty.npy'
+TARGET_HDF5_FILE='../../data/ANP/processed/area_1/porosity_data.h5'
 
-python3 wells_data3_hdf5.py --base-feat-file  $FEAT_FILE --porosity-file-path  $POR_FILE --hdf5-file-path $TARGET_HDF5_FILE
+python3 wells_data3_hdf5.py --f  $FEAT_FILE --p  $POR_FILE --o $TARGET_HDF5_FILE
