475e943585d47e0d27ced8163c64c8d7146474ce
Daniel Campos <danielcampos14@hotmail.com.br>
2023-07-17 11:19:49 -0300

    test: test WellsDataFilter

--- src/alg/tests/test_data_filter.py
+++ src/alg/tests/test_data_filter.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
-from data_filter import *
+from data_filter import DataFilter, FeatSelectionTrainDataFilter
+from data_filter import PredTrainDataFilter, WellsDataFilter
 from common import RealValues
 import h5py
 import tempfile
@@ -202,6 +203,62 @@ class TestTrainDataFilters(TestCase):
         self.h5_file.close()
         self.tmp_file.close()
 
+
+class TestWellsDataFilter(TestCase):
+
+    def setUp(self) -> None:
+        self.tmp_file = tempfile.TemporaryFile()
+        self.h5_file = h5py.File(self.tmp_file, 'a')
+
+        self.cur_data_type = [('x', np.int64), ('y', np.int64),
+                              ('z', np.int64), ('phi', np.float64),
+                              ('well_id', np.int64), ('ring', np.int8)]
+        self.cur_data_type = np.dtype(self.cur_data_type)
+        #Dont need a big dataset here
+        x_size = 30
+        y_size = 10
+        z_size = 10
+        phi_size = 10
+        well_id_size = 10
+        ring_size = 1
+        data = np.empty(
+            (x_size, y_size, z_size, phi_size, well_id_size, ring_size),
+            dtype=self.cur_data_type)
+
+        middle_y = y_size // 2
+        for i in range(x_size):
+            for j in range(y_size):
+                data[i, j]['x'] = i
+                data[i, j]['y'] = j
+                data[i, j]['z'] = np.arange(10)[:, np.newaxis]
+                data[i, j]['phi'] = np.random.rand(z_size, phi_size,
+                                                   well_id_size, ring_size)
+                #The cube has x ranges associated with a well
+                #example: if x in [0, 10], well = 0,
+                #example: if x in [40, 50], well = 4
+                #There is going to be x_size//10 wells
+                data[i, j]['well_id'] = np.array([i // 10] * 10)[:, np.newaxis]
+
+                #The ring num is based from the middle y
+                data[i, j]['ring'] = abs(middle_y - j)
+
+        self.dset = self.h5_file.create_dataset("default",
+                                                dtype=self.cur_data_type,
+                                                data=data,
+                                                chunks=(10, 10, 10, 10, 10, 1))
+
+        #There are 3 wells in total, 2 of them are test wells
+        target_wells = [0, 1]
+        self.data_filter = WellsDataFilter(target_wells)
+    
+    def test_can_filter_automatically(self):
+        result_size = len(self.data_filter.filter(self.dset[:]))
+        self.assertTrue(result_size > 0 and result_size < self.dset.size)
+
+    def tearDown(self):
+        #this order matters
+        self.h5_file.close()
+        self.tmp_file.close()
 
 if __name__ == "__main__":
     main()
