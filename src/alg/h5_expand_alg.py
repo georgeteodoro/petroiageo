a5db1da156f8e452ad3ffbddb52f9c25e359bd16
Daniel Campos <danielcampos14@hotmail.com.br>
2023-08-15 08:08:12 -0300

    bug-fix: expand only training wells

--- src/alg/h5_expand_alg.py
+++ src/alg/h5_expand_alg.py
@@ -27,7 +27,8 @@ class H5ExpandAlg(AbstractExpandAlg):
         Expand training wells
         """
         # Retrieve config parameters
-        wells_coords = self._config.wells_as_simple_list
+        wells_coords = self._config.train_wells_coords
+        wells_coords = self._config.train_wells_coords
         comm = self._config.get_param('mpi_global_comm')
         rank = self._config.get_param('mpi_rank')
         should_update = self._config.get_param('mpi_should_update_local')
@@ -119,8 +120,147 @@ class H5ExpandAlg(AbstractExpandAlg):
 
                 ran_chunks += 1  # Used only for profiling
 
-                # Calculate whether the current chunk has any points to
-                # update/expand
+                    within_chunk_cond = (lambda d: (d['x'] >= chunk_x_left) &
+                                         (d['x'] <= chunk_x_right) &
+                                         (d['y'] >= chunk_y_top) &
+                                         (d['y'] <= chunk_y_bot))
+
+                    # Update 'empty' values to 'expanded' if point is
+                    # on any ring border and if they are present on
+                    # this chunk
+                    local_cond = lambda d: within_chunk_cond(d) & (
+                        left_wall_cond(d)
+                        | right_wall_cond(d)
+                        | top_wall_cond(d)
+                        | bot_wall_cond(d))
+
+                    hdf5_util.conditional_map_h5_chunk(
+                        porosity_data_h5,
+                        lambda d:
+                        (d['real'] == common.RealValues.empty) & local_cond(d),
+                        [
+                            ('well_id', well_id),
+                            ('real', common.RealValues.expanded),
+                        ],
+                        chunk_slice,
+                    )
+
+                    hdf5_util.conditional_map_h5_chunk(
+                        porosity_data_h5,
+                        lambda d: (d['real'] == common.RealValues.canal)
+                        & local_cond(d),
+                        [
+                            ('well_id', well_id),
+                            ('real', common.RealValues.expanded),
+                        ],
+                        chunk_slice,
+                    )
+
+                    t3 = time()
+                    total_chunk_update_time += t3 - t2
+                    profiling.prof_expand_chunk_time(it, well_id, n_chunk,
+                                                     t3 - t2)
+
+            profiling.prof_expand_chunks_time(it, total_chunk_update_time,
+                                              self._config)
+            profiling.prof_expand_chunks_ran(it, ran_chunks, total_chunks,
+                                             self._config)
+
+        else:
+            my_rank = rank
+            print(f"[main][{it}][R{my_rank}] waiting points expansion")
+
+        comm.Barrier()
+
+    def expand_points_improved_locality(self, porosity_data_h5: h5py.Dataset,
+                                        it: int):
+        # Retrieve config parameters
+        wells_coords = self._config.wells_as_simple_list
+        comm = self._config.get_param('mpi_global_comm')
+        rank = self._config.get_param('mpi_rank')
+        should_update = self._config.get_param('mpi_should_update_local')
+        full_depth_chunks = self._config.get_param('full_depth_chunks')
+        window_size = self._config.get_param('window')
+
+        # Only one process per node is required to update
+        if should_update:
+            ring = it
+
+            t1 = time()
+
+            print(f"[gen_expanded_points][it{it}] "\
+                  f"Hypercube shape: {porosity_data_h5.shape}")
+
+            print(f"[gen_expanded_points][it{it}] "\
+                  f"Expanding points on ring {ring}")
+
+            total_chunk_update_time = 0
+
+            # TODO: add progress bar later
+            # Iterate on all chunks
+            for chunk_slice in porosity_data_h5.iter_chunks():
+
+                chunk_x_left = chunk_slice[0].start
+                chunk_x_right = chunk_slice[0].stop - 1
+                chunk_y_top = chunk_slice[1].start
+                chunk_y_bot = chunk_slice[1].stop - 1
+
+                # Create a list of wells which have
+                # points within the current chunk
+                wells_to_update = []
+                for well_id, well in enumerate(wells_coords):
+                    # Calculate the coordinates of the current well-ring
+                    well_ring_x_left = well[0] - ring
+                    well_ring_x_right = well[0] + ring
+                    well_ring_y_top = well[1] - ring
+                    well_ring_y_bot = well[1] + ring
+
+                    # Check if any point in the well-ring is within the chunk
+                    ovlp_x = (chunk_x_right >= well_ring_x_left) & (
+                        chunk_x_left <= well_ring_x_right)
+                    ovlp_y = (chunk_y_bot >= well_ring_y_top) & (
+                        chunk_y_top <= well_ring_y_bot)
+
+                    # If the chunk region is withing a well-ring the overlap
+                    # conditions are true, but there is no overlap.
+                    well_ring_not_bigger = (
+                        well_ring_x_left >= chunk_x_left | well_ring_x_right <=
+                        chunk_x_right | well_ring_y_top >=
+                        chunk_y_top | well_ring_y_bot <= chunk_y_bot)
+
+                    # full_depth_chunks: whether the chunks for
+                    # porosity_data_h5 includes the full depth, i.e.,
+                    # there are no 2 chunks which are stacked upon each other.
+                    # This allows faster checking for well/chunk overlaps
+                    if full_depth_chunks:
+                        if (ovlp_x & ovlp_y) & well_ring_not_bigger:
+                            wells_to_update.append(well_id)
+                    else:
+                        print(
+                            "[expand4_hdf5] Not using full_depth_chunks=True")
+                        raise NotImplementedError
+
+                # Update the values of each well point withing the current chunk
+                for well_id in wells_to_update:
+                    # Calculate the coordinates of the current well-ring
+                    well_ring_x_left = wells_coords[well_id][0] - ring
+                    well_ring_x_right = wells_coords[well_id][0] + ring
+                    well_ring_y_top = wells_coords[well_id][1] - ring
+                    well_ring_y_bot = wells_coords[well_id][1] + ring
+
+                    # Conditions for points on each ring wall
+                    left_wall_cond = (lambda d: (d['x'] == well_ring_x_left)
+                                      & (d['y'] <= well_ring_y_bot)
+                                      & (d['y'] >= well_ring_y_top))
+                    right_wall_cond = (lambda d: (d['x'] == well_ring_x_right)
+                                       & (d['y'] <= well_ring_y_bot)
+                                       & (d['y'] >= well_ring_y_top))
+                    top_wall_cond = (lambda d: (d['y'] == well_ring_y_top)
+                                     & (d['x'] <= well_ring_x_right)
+                                     & (d['x'] >= well_ring_x_left))
+                    bot_wall_cond = (lambda d: (d['y'] == well_ring_y_bot)
+                                     & (d['x'] <= well_ring_x_right)
+                                     & (d['x'] >= well_ring_x_left))
 
                 # Update 'empty' values to 'expanded' if point is
                 # on any ring border
