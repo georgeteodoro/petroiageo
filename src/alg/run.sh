e254b87d05b8a0bd17ed62ab26a414942ee85192
Daniel Campos <danielcampos14@hotmail.com.br>
2024-02-15 09:50:13 -0300

    improve script

--- src/alg/run.sh
+++ src/alg/run.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1		    #Numero de Nós
 #SBATCH -p ict_cpu		    #Fila (partition) a ser utilizada
 #SBATCH -J area_1_job	    #Nome job
-#SBATCH --time=24:00:00
+#SBATCH --time=01:00:00
 #SBATCH --account=petrobrasiageo
 #SBATCH --exclusive		    #Utilização exclusiva dos nós
 #SBATCH --no-requeue	    # Não ressubmete um job se ele der erro
@@ -14,17 +14,20 @@ set -x
 
 ORIG=$(pwd)
 
-module load python/3.9.6
+# module load python/3.9.6
 module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
+module load gcc/6.5_sequana
 
 # When using a virtual environment, which is highly recommended
-VENV_PATH=./venv
-source $VENV_PATH/bin/activate
-python3 -m pip list
+# VENV_PATH=../venv
+# source $VENV_PATH/bin/activate
+python3 -m pip list -v
 
-ORIG_HDF5_FEATS_FOLDER=../../data/ANP/features/area1/hdf5/
-ORIG_POR_DATA_PATH=../../data/ANP/processed/area_1/porosity_data.h5
+ORIG_HDF5_FEATS_FOLDER=/petrobr/parceirosbr/petrobrasiageo/daniel.campos/data/ANP/area1/hdf5/
+ORIG_POR_DATA_PATH=/petrobr/parceirosbr/petrobrasiageo/daniel.campos/petroiageo/data/ANP/processed/area_1/porosity_data.h5
 TARGET_TMP_POR_FILE_NAME=/tmp/test/porosity_data.h5
+
+ON_LOCAL=0
 
 if [ $ON_LOCAL -eq 1 ] 
     then
@@ -47,7 +50,7 @@ cd /tmp/test
 CONFIG_FILE='./area1_config.yaml'
 #If there are a lot of features inside the features folder specified in the config file, this option reduces the number of features loaded for the alg.
 #This is mainly for testing purposes as for a real run we should consider all features possible.
-NUM_FEATS_TO_CONSIDER=4
+# NUM_FEATS_TO_CONSIDER=4
 
 FILENAME="run_log"
 LOG_FILE_NAME=${BASE}/${FILENAME}.mem.log
@@ -58,11 +61,11 @@ rm -f $FILENAME
 # The iteration starts at 1
 IT_START=1
 # Number of iterations to run beggining at IT_START
-N_ITS=22
+N_ITS=25
 # The window used to consider on top of base features
 WINDOW_USED=3
 # Number of processes/workers per node to be used
-N_PROCS=8
+N_PROCS=10
 
 # See python3 -u main.py --help for args list 
 vmstat 5 -S M -t -w >> $LOG_FILE_NAME & mpirun -np $(($SLURM_JOB_NUM_NODES * $N_PROCS)) --map-by node --tag-output --bind-to core --oversubscribe python3 -u main.py --config $CONFIG_FILE --it $IT_START --nits $N_ITS -w $WINDOW_USED | tee $FILENAME.log
--- src/data/filter_seismic_on_wells.py
+++ src/data/filter_seismic_on_wells.py
@@ -10,22 +10,35 @@ SEISMIC_FILE_RESOLUTION = 5
 
 if __name__ == "__main__":
     # Como o arquivo sísmico tem medições à cada 5 metros e a profundidade
-    # alvo da porosidade é entre 5651 e 5810 metros, tenho que dividir por 5
-    # esse range para chegar no range correto no arquivo sísmico.
-    # O z_end não vai ser incluso
-    z_start, z_end = 5651 // SEISMIC_FILE_RESOLUTION, 5810 // SEISMIC_FILE_RESOLUTION
-    
+    # alvo da porosidade é entre TARGET_Z_MIN e TARGET_Z_MAX metros, tenho que dividir por
+    # SEISMIC_FILE_RESOLUTION esse range para chegar no range correto no arquivo sísmico.
+    # Por causa disso, pode ser que o valor de profundidade dos extremos seja diferente
+    # do TARGET_Z_MIN e TARGET_Z_MAX informados
+    TARGET_Z_MIN = 5331
+    TARGET_Z_MAX = 6030
+    z_start, z_end = TARGET_Z_MIN // SEISMIC_FILE_RESOLUTION, TARGET_Z_MAX // SEISMIC_FILE_RESOLUTION
+    print(f"Intervalo de dados original pretendido: [{z_start}, {z_end}], {z_end-z_start+1} valores por poço.")
+ 
     # Where to read the seismic values from
     origin_npy_file_path = pathlib.Path(
         "/petrobr/parceirosbr/petrobrasiageo/dados/Franco_florin_buzios_28_09-2.npy"
         )
     print(f"Carregando dados de {origin_npy_file_path}")
-    z_filtered_np = np.load(origin_npy_file_path)[:,:,z_start : z_end]
+    z_filtered_np = np.load(origin_npy_file_path)
+    print(f"Shape do arquivo sísmico original: {z_filtered_np.shape}")
+    max_z_to_filter_in_seismic = z_filtered_np.shape[-1]
+    if max_z_to_filter_in_seismic < z_end+1:
+        print(f"O arquivo sísmico tem z máximo de {max_z_to_filter_in_seismic}" +
+            f" mas o z_end alvo é de {z_end+1}. Filtrando pelo mínimo.")
+    
+    z_end_to_filter = min(z_filtered_np.shape[-1], z_end+1)
+    print(f"Filtrando em z no intervalo [{z_start},{z_end_to_filter}), {z_end_to_filter-z_start} valores por poço.")
+    z_filtered_np = z_filtered_np[:,:,z_start : z_end_to_filter]
     print(f"Depois de filtrar por z, tem o shape de: {z_filtered_np.shape}")
 
     # Where to save the filtered values
     target_npy_file_path = pathlib.Path(
-        "/petrobr/parceirosbr/petrobrasiageo/dados/Franco_florin_buzios_wells_filtered.npy"
+        "/petrobr/parceirosbr/petrobrasiageo/dados/Franco_florin_buzios_wells_filtered_area_1.npy"
         )
 
     target_x_y_coords = [
@@ -34,18 +47,22 @@ if __name__ == "__main__":
         [833, 1384], 
         [898, 1246], 
         [901, 1332]]
-
+    
+    n_wells = len(target_x_y_coords)
+    print(f"{n_wells} poços a serem filtrados nas coordenadas: {target_x_y_coords}")
     resulting_np = None
 
-    for x, y in target_x_y_coords:
+    for well_id, (x, y) in enumerate(target_x_y_coords):
         filtered_np = z_filtered_np[x, y, :]
-        print(f"Filtrou np com shape: {filtered_np.shape}")
+        print(f"Poço {well_id+1}/{n_wells}. Filtrou np com shape: {filtered_np.shape}")
         # Create x,y,z,seismic numpy array, i.e., 
         # a (4, filtered_np.shape[-1]) shaped array
         n_values = filtered_np.shape[-1]
         xs = np.array([x]*n_values).reshape(n_values, 1)
         ys = np.array([y]*filtered_np.shape[-1]).reshape(n_values, 1)
-        zs = np.arange(z_start, z_end, 1).reshape(n_values, 1) * SEISMIC_FILE_RESOLUTION
+        # The zs are in the original depth, thats why we multiply
+        # it back with SEISMIC_FILE_RESOLUTION
+        zs = np.arange(z_start, z_end_to_filter, 1).reshape(n_values, 1) * SEISMIC_FILE_RESOLUTION
         # Goes from (shape, ) to (shape, 1)
         filtered_np = filtered_np.reshape(n_values, 1)
         curr_np = np.concatenate([xs, ys, zs, filtered_np], axis=1)
@@ -58,4 +75,9 @@ if __name__ == "__main__":
         print(f"Novo np resultante de shape {resulting_np.shape}")
     
     print(f"Tentando salvar em {target_npy_file_path}")
-    np.save(target_npy_file_path, resulting_np)
+    try:
+        np.save(target_npy_file_path, resulting_np)
+    except Exception as e:
+        print("Erro ao salvar:\n"+e)
+    else:
+        print("Salvo com sucesso")
