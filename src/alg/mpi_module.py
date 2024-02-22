dd2f6fbad8fa17eafe1df461e1b4d06166960597
Willian <guns945@gmail.com>
2024-02-22 16:19:46 -0300

    Added a new config parameter: the mpi ranks mapping by node.

--- src/alg/mpi_module.py
+++ src/alg/mpi_module.py
@@ -35,7 +35,7 @@ class MPI_TAGS(Enum):
 
 
 def _get_local_node_comm(comm):
-    """
+    '''
     Return a split communicator for processes within the same node.
     For instance, running 4 nodes and 8 processes would yield 4 different
     communicators with 2 processes.
@@ -43,7 +43,7 @@ def _get_local_node_comm(comm):
     This is required for using distributed hdf5. The MPI_File_open routine
     needs the same file for the input MPI communicator, and for running
     locally the files although having the same name and path, are different.
-    """
+    '''
 
     # Get all node names
     local_host_name = MPI.Get_processor_name()
@@ -70,13 +70,13 @@ def _get_local_node_comm(comm):
 
 
 def _should_update_local(mpi_size, rank, comm):
-    """
+    '''
     Check whether the current process should update the local
     h5 porosity file.
     Only one process per node should do this.
     Although multiple updates works on h5, it is inefficient.
     Manager process don't perform propagation.
-    """
+    '''
 
     ret = False
     assigned_nodes = []
@@ -99,10 +99,30 @@ def _should_update_local(mpi_size, rank, comm):
 
     return ret
 
+def _get_mapping(rank, mpi_size, manager_rank, comm):
+    
+    if rank == manager_rank:
+        tmp_rank_mapping = dict()    
+        for i in range(mpi_size-1):
+            r, node = comm.recv()
+            if node in tmp_rank_mapping:
+                tmp_rank_mapping[node].append(r)
+            else:
+                tmp_rank_mapping[node] = [r]
+        
+        # Change node name to an uid
+        rank_mapping = dict()
+        for nid, ranks in enumerate(tmp_rank_mapping.values()):
+            rank_mapping[nid] = ranks
+        
+        comm.bcast(rank_mapping, root=manager_rank)
+    else:
+        comm.send((rank, MPI.Get_processor_name()),dest=manager_rank)
+        rank_mapping = comm.bcast(None, root=manager_rank)
+
+    return rank_mapping
 
 def initialize(config: config_parser.Config):
-    """ """
-
     # Get base MPI variables
     global_comm = MPI.COMM_WORLD
     rank = global_comm.Get_rank()
@@ -111,11 +131,13 @@ def initialize(config: config_parser.Config):
 
     local_comm = _get_local_node_comm(global_comm)
     should_update_local = _should_update_local(mpi_size, rank, global_comm)
+    rank_mapping = _get_mapping(rank, mpi_size, manager_rank, global_comm)
 
     # TODO: later add a 'mpi' TOP_LEVEL_BASE_CONFIG to config
-    config.add_param("mpi_global_comm", global_comm)
-    config.add_param("mpi_rank", rank)
-    config.add_param("mpi_size", mpi_size)
-    config.add_param("mpi_manager_rank", manager_rank)
-    config.add_param("mpi_local_comm", local_comm)
-    config.add_param("mpi_should_update_local", should_update_local)
+    config.add_param('mpi_global_comm', global_comm)
+    config.add_param('mpi_rank', rank)
+    config.add_param('mpi_size', mpi_size)
+    config.add_param('mpi_manager_rank', manager_rank)
+    config.add_param('mpi_local_comm', local_comm)
+    config.add_param('mpi_should_update_local', should_update_local)
+    config.add_param('mpi_rank_mapping', rank_mapping)
