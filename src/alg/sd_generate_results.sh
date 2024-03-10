adc8cab295985febd0c76d961fc3473e5d372b12
Daniel Campos <danielcampos14@hotmail.com.br>
2024-03-10 10:21:30 -0300

    update generate results script

--- src/alg/sd_generate_results.sh
+++ src/alg/sd_generate_results.sh
@@ -12,13 +12,12 @@
 TMP_FILE=filtered_log.log
 
 FIELD_SEP=";"
-# We must filter by the first node on SD to remove duplicated messages
-# grep "\[1,0\]" $1 | cut --d ":" -f 1 --complement >> $TMP_FILE
-cat $1 > $TMP_FILE
+
+cut --d ":" -f 1 --complement $1 > $TMP_FILE
 
 # Get the expected errors and feats selected per it
-grep '\[manager\]' $TMP_FILE | grep 'Iteration best features' |
-tr -d "[]" | sed -e 's/managerit//' -e "s/ Iteration best features: /$FIELD_SEP/" \
+grep 'Iteration best features' $TMP_FILE |
+tr -d "[]" | sed -e "s/managerit//" -e "s/ Iteration best features: /$FIELD_SEP/" \
 -e "s/ with errors: MAE /$FIELD_SEP/" -e "s/ RMSE /$FIELD_SEP/" |
 awk -v OFS=$FIELD_SEP -v FS=$FIELD_SEP 'BEGIN{print "iteration", "RMSE", "MAE", "features"} {print $1, $4, $3, $2}' > $2
 
@@ -26,11 +25,11 @@ awk -v OFS=$FIELD_SEP -v FS=$FIELD_SEP 'BEGIN{print "iteration", "RMSE", "MAE", 
 grep 'Test errors:' $TMP_FILE | tr -d "[]" |
 sed -e 's/propagationit//' -e "s/ Test errors: RMSE: /$FIELD_SEP/" \
 -e "s/ MAE: /$FIELD_SEP/" |
-awk -v FS=$FIELD_SEP -v OFS=$FIELD_SEP 'BEGIN{print "iteration", "RMSE", "MAE"} {print $0}' > $3  
+awk -v FS=$FIELD_SEP -v OFS=$FIELD_SEP 'BEGIN{print "iteration", "RMSE", "MAE"} {print $0}' | uniq >  $3  
 
 # Get the total time per it
-grep 'Iteration total time(s)' $TMP_FILE | tr -d "[]" |
-sed -e 's/worker0it//' -e "s/ Iteration total time(s): /$FIELD_SEP/" |
+grep '\[1,0\]' $1 | cut --d ":" -f 1 --complement | grep 'Iteration total time(s)' | tr -d "[]" |
+sed -e 's/worker[[:digit:]]*it//' -e "s/ Iteration total time(s): /$FIELD_SEP/" |
 awk -v FS=$FIELD_SEP -v OFS=$FIELD_SEP 'BEGIN{print "iteration", "seconds"} {print $0}' > $4
 
 # Dont need it anymore
