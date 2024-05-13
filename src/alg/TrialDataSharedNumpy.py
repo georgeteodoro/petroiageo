b8a8def64cb4e50acd3d56ed781c58b4c30ac891
Willian <guns945@gmail.com>
2024-05-13 18:54:46 -0300

    Fixed empty ring case for TrialDataShared.

--- src/alg/TrialDataSharedNumpy.py
+++ src/alg/TrialDataSharedNumpy.py
@@ -15,14 +15,16 @@ class TrialDataSharedNumpy(TrialDataSharedBase):
     Shared memory implementation through numpy.
 
     '''
+
     def __init__(self,
                  target_wells_list,
                  porosity_data,
                  config,
                  should_consider_sampling: bool = True):
         # Currently no initialization is needed
-        super(TrialDataSharedNumpy, self).__init__(
-            target_wells_list, porosity_data, config, should_consider_sampling)
+        super(TrialDataSharedNumpy,
+              self).__init__(target_wells_list, porosity_data, config,
+                             should_consider_sampling)
 
         # Shared memory objects used to get data for numpy objects
         # (self._data_shr). This is necessary for cleanup on __del__().
@@ -56,11 +58,16 @@ class TrialDataSharedNumpy(TrialDataSharedBase):
         # Only the space for a single column is allocated.
         return np.zeros((length), dtype=np.float64)
 
-    def _alloc_empty_ring_well_concrete(self, length, last_feature=False):
+    def _alloc_empty_ring_well_concrete(self, length):
         '''
         Allocate an empty concrete object to store shared data for a
         ring/well pair, or the single column for the current feature.
         '''
+
+        # On the case of an empty ring,well pair, just create an empty
+        # numpy array with the correct shape/dtype.
+        if length == 0:
+            return np.zeros((0), dtype=self._cur_data_type)
 
         # Only a single responsible rank allocates the shared memory region
         if self._mpi_local_comm is None or self._is_resp_rank:
