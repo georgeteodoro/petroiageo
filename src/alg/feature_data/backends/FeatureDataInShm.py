32cb1f3537259af792b0d893953502c0a6f7ab64
WillianJunior <guns945@gmail.com>
2024-06-28 15:06:57 -0300

    added feature cache miss profiling.

--- src/alg/feature_data/backends/FeatureDataInShm.py
+++ src/alg/feature_data/backends/FeatureDataInShm.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import h5py
 import fasteners  # inter-process, intra-node lock
 from multiprocessing import shared_memory
+from time import time
 
 from feature_data.backends.FeatureDataBase import FeatureDataBase
 import common
@@ -19,6 +20,13 @@ class FeatureDataInShm(FeatureDataBase):
     is valid, then the feature data is read on the shared-memory location
     from shm_path. Otherwise, it is assumed that the data is already on 
     shm_path. Either way, an np.ndarray wraps the shared-memory data.
+    This means: CALLER SHOULD SYNC CREATION!!!
+    If feature_path==None then data from H5 file is copied into the shared
+    memory space.
+
+    NO ALLOCATION IS DONE HERE!!! Shared memory space should already be
+    allocated. Here the numpy interface is acquired and H5 data is copied,
+    if necessary.
 
     On feature loading, a write lock is acquired, released after the 
     feature is loaded. Before the np.ndarray creation, a read lock is 
@@ -55,20 +63,24 @@ class FeatureDataInShm(FeatureDataBase):
             # and reading the file.
             feature_file_name = feature_path[feature_path.rfind('/') + 1:]
             feature_name = feature_file_name[:feature_file_name.find('.')]
-            self._feature_file = h5py.File(feature_path, 'r')
+            feature_file = h5py.File(feature_path, 'r')
 
-            assert self._feature_file is not None, "[FeatureDataInShm] "\
+            assert feature_file is not None, "[FeatureDataInShm] "\
                 f"Could not open file {feature_path}"
 
-            feature_dset = self._feature_file[common.FEAT_DSET_NAME]
+            feature_dset = feature_file[common.FEAT_DSET_NAME]
 
             assert feature_dset is not None, "[FeatureDataInShm] "\
                 f"Could not get dataset {FEAT_DSET_NAME} of file {feature_path}"
 
             # Pre-fetch all data
             # print(f"[FeatureDataInShm][__init__] Fetching feature data")
+            t0 = time()
             self._feature[:] = feature_dset[:]
-            self._feature_file.close()
+            t1 = time()
+            print(f"[FeatureDataInShm][__init__] Feature {feature_name} "
+                  f"loaded in {t1-t0:.4f}")
+            feature_file.close()
 
             # print(f"[FeatureDataInShm][__init__] Releasing Write lock")
             self._lock.release_write_lock()
