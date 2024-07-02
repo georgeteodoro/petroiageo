d8edc4253e37e602a0ea5a0fd7afd38750dca479
Willian <guns945@gmail.com>
2024-07-02 14:08:34 -0300

    New memfill algorithm for mem-pressure tests.

---
+++ src/mem_fill.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+	long size_gb = atoi(argv[1]);
+
+	long num_allocs = 1;
+	if (argc == 3) {
+		num_allocs = atoi(argv[2]);
+	}
+
+	long alloc_size = size_gb * 1024 * 1024 * 1024 / sizeof(long) / num_allocs;
+
+	long *allocs[num_allocs];
+	
+	printf("alloc size: %ld\n", alloc_size);
+
+	for (int i=0; i<num_allocs; i++) {
+		printf("alloc-ing: %ld longs\n", alloc_size);
+		allocs[i] = malloc(alloc_size * sizeof(long));
+		for (int j=0; j<alloc_size; j++){
+			allocs[i][j] = 1;
+		}
+	}
+
+	// Sleep forever...
+	for (;;) pause();
+
+	return 0;
+}
