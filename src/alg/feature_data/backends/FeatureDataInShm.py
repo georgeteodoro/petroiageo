58e23c562c229a73fb8e6085d40f7590118bbb1b
Willian <guns945@gmail.com>
2024-07-18 12:28:21 -0300

    .

--- src/alg/feature_data/backends/FeatureDataInShm.py
+++ src/alg/feature_data/backends/FeatureDataInShm.py
@@ -2,7 +2,7 @@ import numpy as np
 from abc import ABC, abstractmethod
 import fasteners  # inter-process, intra-node lock
 from multiprocessing import shared_memory
-from time import time
+from time import time, sleep
 import psutil
 
 from feature_data.backends.FeatureDataBase import FeatureDataBase
@@ -72,19 +72,21 @@ class FeatureDataInShm(FeatureDataBase):
             print(f"[FeatureDataInShm][__init__] Feature {feature_name} "
                   f"loading... - "
                   f"{psutil.virtual_memory()}")
+            sleep(5)
             for i in range(feature_data.shape[0]):
                 self._feature[i, :] = feature_data[i]
             t1 = time()
-            time.sleep(5)
             print(f"[FeatureDataInShm][__init__] Feature {feature_name} "
                   f"loaded in {t1-t0:.4f} - "
                   f"{psutil.virtual_memory()}")
-            time.sleep(5)
+            sleep(5)
             del feature_data
-            time.sleep(5)
+            sleep(5)
             print(f"[FeatureDataInShm][__init__] Feature {feature_name} "
                   f"after del - "
                   f"{psutil.virtual_memory()}")
+            sleep(5)
+            0/0
             self._close_feature_file()
 
             # print(f"[FeatureDataInShm][__init__] Releasing Write lock")
