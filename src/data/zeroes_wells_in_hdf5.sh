5dfc061b88fdb57fcc7530edf7a181c69692e93d
Daniel Campos <danielcampos14@hotmail.com.br>
2024-08-27 17:52:13 -0300

    zeroes wells in hdf5 sh script

---
+++ src/data/zeroes_wells_in_hdf5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --nodes=1           #Numero de Nó
+#SBATCH -p ict_cpu       #Fila (partition) a ser utilizada
+#SBATCH -J por_gen       #Nome job
+#SBATCH --time=1:00:00
+#SBATCH --account=petrobrasiageo
+#SBATCH --exclusive         #Utilização exclusiva dos nós
+
+#module load python/3.9.1
+module load /scratch/app/modulos/sequana/current openmpi/gnu/4.0.1_sequana
+
+source /petrobr/parceirosbr/petrobrasiageo/daniel.campos/venv-py3.12/bin/activate
+
+python3 --version
+which python3
+python3 -m pip list -v
+
+INPUT='/petrobr/parceirosbr/petrobrasiageo/daniel.campos/data/ANP/v2/area1/cube/cube_4_train_wells_baseline.h5'
+WELLS_IDXS=5
+CONFIG='/petrobr/parceirosbr/petrobrasiageo/daniel.campos/petroiageo/src/alg/configs/ANP/v2/area1/exp2/area_1_config.yaml'
+
+python3 zeroes_wells_in_hdf5.py --input $INPUT --config $CONFIG --wells_idxs $WELLS_IDXS
