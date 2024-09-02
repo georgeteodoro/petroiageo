f367e1df2c0c857fed04117ca198c6221d48d80a
Daniel Campos <danielcampos14@hotmail.com.br>
2024-09-02 17:26:24 -0300

    bug-fix: Remove usage of window when propagating

--- src/alg/propagate.py
+++ src/alg/propagate.py
@@ -89,15 +89,12 @@ def propagate(porosity_data_h5: Dataset, trial_data: TrialDataBase,
         #       f"cur_chunk_np hash: {hash(cur_chunk_np.data.tobytes())}")
 
         # Propagate the points of each well
-        window_size = config.alg["window"]
-        hypercube_shape = porosity_data_h5.shape
         for w_x, w_y in wells_to_update:
             # print(f"[propagation1][it{it}][worker{rank}][chunk{chunk_n}] "
             #       f"well: {(w_x, w_y)}")
 
-            coords_to_update = _get_coords_to_propagate(window_size,
-                                                        hypercube_shape, ring,
-                                                        cur_chunk_np, w_x, w_y)
+            coords_to_update = _get_coords_to_propagate(ring, cur_chunk_np, w_x,
+                                                        w_y)
 
             n_propagated_points += len(coords_to_update[0])
 
@@ -126,8 +123,7 @@ def propagate(porosity_data_h5: Dataset, trial_data: TrialDataBase,
     return n_propagated_points
 
 
-def _get_coords_to_propagate(window_size: int, hypercube_shape: tuple,
-                             ring: int, data: np.ndarray, w_x: int,
+def _get_coords_to_propagate(ring: int, data: np.ndarray, w_x: int,
                              w_y: int) -> np.ndarray:
     # Calculate the coordinates of the current well-ring
     well_ring_x_left = w_x - ring
@@ -149,22 +145,12 @@ def _get_coords_to_propagate(window_size: int, hypercube_shape: tuple,
                      & (d['x'] <= well_ring_x_right)
                      & (d['x'] >= well_ring_x_left))
 
-    # Condition to avoid padding region since the displacement
-    # may result in out of bounds access.
-    not_on_padding = (lambda d: (d['z'] >= window_size) &
-                      (d['z'] < hypercube_shape[2] - window_size) &
-                      (d['y'] >= window_size) &
-                      (d['y'] < hypercube_shape[1] - window_size) &
-                      (d['x'] >= window_size) &
-                      (d['x'] < hypercube_shape[0] - window_size))
-
     # Only empty points can be propagated
     filter_fun = lambda d: (d['real'] == common.RealValues.empty) \
                                  & (left_wall_cond(d) \
                                   | right_wall_cond(d) \
                                   | top_wall_cond(d) \
-                                  | bot_wall_cond(d)) \
-                                 & not_on_padding(d)
+                                  | bot_wall_cond(d))
     coords_to_update = np.where(filter_fun(data))
     return coords_to_update
 
--- src/alg/tests/test_propagate.py
+++ src/alg/tests/test_propagate.py
@@ -21,8 +21,8 @@ class TestPropagate(TestCase):
         x_size = 5
         y_size = 5
         z_size = 5
-        self.data = np.empty((x_size, y_size, z_size),
-                             dtype=cur_data_type)
+        #Each point in the cube has 4 dimensions
+        self.data = np.empty((x_size, y_size, z_size), dtype=cur_data_type)
         # Fill cube
         for x in range(x_size):
             all_yz = []
@@ -31,37 +31,70 @@ class TestPropagate(TestCase):
                 for z in range(z_size):
                     all_z = all_z + [(x, y, z, RealValues.empty)]
                 all_yz = all_yz + [all_z]
-            
+
             self.data[x, ...] = all_yz
 
         # Mark wells points
-        self.wells_coords = [(1, 1), (3, 4)]
+        self.wells_coords = [(1, 1), (4, 3)]
         for (x, y) in self.wells_coords:
-            all_z = [() for _ in range(z_size)] 
+            all_z = [() for _ in range(z_size)]
             for z in range(z_size):
                 all_z[z] = (x, y, z, RealValues.real)
             self.data[x, y, ...] = all_z
 
     def test_can_get_coords_to_propagate_ring_0_full_z(self):
+        ring_to_expand = 0
         for well_x, well_y in self.wells_coords:
-            target_coords = _get_coords_to_propagate(0, (5, 5, 5), 0, self.data,
-                                                     well_x,
-                                                     well_y)
+            target_coords = _get_coords_to_propagate(ring_to_expand, self.data,
+                                                     well_x, well_y)
             self.assertTrue(len(target_coords[0]) == 0)
-    
+
     def test_can_get_coords_to_propagate_ring_0_not_full_z(self):
+        ring_to_expand = 0
         for well_x, well_y in self.wells_coords:
             #Mark first depth of well as not real
             self.data[well_x, well_y, 0]['real'] = RealValues.empty
-            target_coords = _get_coords_to_propagate(0, (5, 5, 5), 0, self.data,
-                                                     well_x,
-                                                     well_y)
+            target_coords = _get_coords_to_propagate(ring_to_expand, self.data,
+                                                     well_x, well_y)
+            #Should return the first depth of well as a target
             expected_target_x = np.array([well_x])
             expected_target_y = np.array([well_y])
             expected_target_z = np.array([0])
             self.assertTrue(np.equal(target_coords[0], expected_target_x))
             self.assertTrue(np.equal(target_coords[1], expected_target_y))
             self.assertTrue(np.equal(target_coords[2], expected_target_z))
+
+    def test_can_get_coords_to_propagate_ring_1(self):
+        ring_to_expand = 1
+        well_to_expected_target_coords = {
+            0: [
+                [0] * 15 + [1] * 10 + [2] * 15,  # First dim
+                [0] * 5 + [1] * 5 + [2] * 5 + [0] * 5 + [2] * 5 + [0] * 5 +
+                [1] * 5 + [2] * 5,  # Second dim
+                [0, 1, 2, 3, 4] * 8
+            ],  # Third dim
+            1: [[3] * 15 + [4] * 10, # First dim
+                [2] * 5 + [3] * 5 + [4] * 5 + [2] * 5 + [4] * 5, # Secong dim
+                [0, 1, 2, 3, 4] * 5] # Third dim
+        }
+        for well_id, (well_x, well_y) in enumerate(self.wells_coords):
+            #Mark first depth of well as not real
+            self.data[well_x, well_y, 0]['real'] = RealValues.empty
+            target_coords = _get_coords_to_propagate(ring_to_expand, self.data,
+                                                     well_x, well_y)
+            #Should return the first depth of well as a target
+            self.assertTrue(
+                np.array_equal(
+                    target_coords[0],
+                    np.array(well_to_expected_target_coords[well_id][0])))
+            self.assertTrue(
+                np.array_equal(
+                    target_coords[1],
+                    np.array(well_to_expected_target_coords[well_id][1])))
+            self.assertTrue(
+                np.array_equal(
+                    target_coords[2],
+                    np.array(well_to_expected_target_coords[well_id][2])))
 
 
 if __name__ == "__main__":
